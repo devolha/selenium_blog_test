04096623f7a05b8b5addc38efad6a38d5e532ab3
devolha <sokolowskaoa@gmail.com>
2025-03-24 12:19:55 +0100

    update: moved methods to the class

--- blog-test.rb
+++ blog-test.rb
@@ -7,25 +7,6 @@ email = "user#{timestamp}@test.com"
 password = "password"
 expected_banner_text = "Welcome to the alpha blog user #{timestamp}"
 
-def enter_username(username)
-	username_field = @driver.find_element(id: 'user_username')
-  	username_field.send_keys(username)
-end
-
-def enter_email(email)
-	email_field = @driver.find_element(id: 'user_email')
-  	email_field.send_keys(email)
-end
-
-def enter_password(password)
-	password_field = @driver.find_element(id: 'user_password')
-  	password_field.send_keys(password)
-end
-
-def submit_form()
-	sign_up_button = @driver.find_element(id: 'submit')
-  	sign_up_button.click
-end
 
 def get_banner_text()
 	banner = @driver.find_element(id: 'flash_success')
--- signup_page.rb
+++ signup_page.rb
@@ -8,4 +8,30 @@ SUBMIT_BUTTON
 
 attr_reader :driver
 
+# class method
+
+def initialize(driver)
+  @driver = driver
+end
+
+def enter_username(username)
+	username_field = @driver.find_element(id: 'user_username')
+  	username_field.send_keys(username)
+end
+
+def enter_email(email)
+	email_field = @driver.find_element(id: 'user_email')
+  	email_field.send_keys(email)
+end
+
+def enter_password(password)
+	password_field = @driver.find_element(id: 'user_password')
+  	password_field.send_keys(password)
+end
+
+def submit_form()
+	sign_up_button = @driver.find_element(id: 'submit')
+  	sign_up_button.click
+end
+
 end
